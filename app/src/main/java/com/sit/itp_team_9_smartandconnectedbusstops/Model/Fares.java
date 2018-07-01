cce6ac555717b458d4cc4bf9f5a68af790cd5216
jlinjy <jlinjyy@gmail.com>
2018-07-01 17:24:31 +0800

    Fares class (not sure if Jeremy still need but just push first lol)

---
+++ app/src/main/java/com/sit/itp_team_9_smartandconnectedbusstops/Model/Fares.java
@@ -0,0 +1,35 @@
+package com.sit.itp_team_9_smartandconnectedbusstops.Model;
+
+public class Fares {
+
+    public double BusMrt;
+    public double ExpressBus;
+    public double MrtBefore745;
+
+    public Fares() {
+    }
+
+    public double getBusMrt() {
+        return BusMrt;
+    }
+
+    public void setBusMrt(double BusMrt) {
+        this.BusMrt = BusMrt;
+    }
+
+    public double getExpressBus() {
+        return ExpressBus;
+    }
+
+    public void setExpressBus(double ExpressBus) {
+        this.ExpressBus = ExpressBus;
+    }
+
+    public double getMrtBefore745() {
+        return MrtBefore745;
+    }
+
+    public void setMrtBefore745(double MrtBefore745) {
+        this.MrtBefore745 = MrtBefore745;
+    }
+}
