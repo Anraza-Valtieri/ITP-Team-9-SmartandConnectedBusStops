fabcd814b8fd498c4c4668326ef2ffbb5ab40d2a
Wenn95 <skye_hweeboon@hotmail.com>
2018-07-08 21:36:30 +0800

    retrieve from twitter

--- app/src/main/java/com/sit/itp_team_9_smartandconnectedbusstops/MainActivity.java
+++ app/src/main/java/com/sit/itp_team_9_smartandconnectedbusstops/MainActivity.java
@@ -13,6 +13,8 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -35,6 +37,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SimpleItemAnimator;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
@@ -76,10 +79,12 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.gson.Gson;
 import com.google.maps.android.clustering.ClusterManager;
 import com.sit.itp_team_9_smartandconnectedbusstops.Adapters.CardAdapter;
 import com.sit.itp_team_9_smartandconnectedbusstops.Adapters.PlaceAutoCompleteAdapter;
 import com.sit.itp_team_9_smartandconnectedbusstops.Model.AdultFares;
+import com.sit.itp_team_9_smartandconnectedbusstops.Model.Authenticated;
 import com.sit.itp_team_9_smartandconnectedbusstops.Model.BusStopCards;
 import com.sit.itp_team_9_smartandconnectedbusstops.Model.Card;
 import com.sit.itp_team_9_smartandconnectedbusstops.Model.DistanceData;
@@ -98,6 +103,23 @@ import com.sit.itp_team_9_smartandconnectedbusstops.Services.NetworkSchedulerSer
 import com.sit.itp_team_9_smartandconnectedbusstops.Utils.FareDetails;
 import com.sit.itp_team_9_smartandconnectedbusstops.Utils.Utils;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
@@ -233,8 +255,10 @@ public class MainActivity extends AppCompatActivity
     private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(
             new LatLng(-1.3520828333333335, -103.81983583333334), new LatLng(1.3520828333333335, 103.8198358333334));
 
-
+    //direction query
     String query;
+    //Twitter username of Mrt updates
+    final static String ScreenName = "SMRT_Singapore";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -344,6 +368,7 @@ public class MainActivity extends AppCompatActivity
         mGeoDataClient = Places.getGeoDataClient(this, null);
         autoCompleteFilter = new AutocompleteFilter.Builder().setCountry("SG").build();
         mPlaceAutoCompleteAdapter = new PlaceAutoCompleteAdapter(MainActivity.this, mGeoDataClient, LAT_LNG_BOUNDS, autoCompleteFilter);
+        downloadTweets();
     }
 
     @Override
@@ -1769,4 +1794,164 @@ public class MainActivity extends AppCompatActivity
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
     }
+
+
+
+    // download twitter timeline after first checking to see if there is a network connection
+    public void downloadTweets() {
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new DownloadTwitterTask().execute(ScreenName);
+        } else {
+            Toast.makeText(getApplicationContext(),"Please check your internet connection",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Uses an AsyncTask to download a Twitter user's timeline
+    private class DownloadTwitterTask extends AsyncTask<String, Void, String> {
+        final static String CONSUMER_KEY = "nW88XLuFSI9DEfHOX2tpleHbR";
+        final static String CONSUMER_SECRET = "hCg3QClZ1iLR13D3IeMvebESKmakIelp4vwFUICuj6HAfNNCer";
+        final static String TwitterTokenURL = "https://api.twitter.com/oauth2/token";
+        final static String TwitterStreamURL = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=";
+        List<String> twitterList = new ArrayList<String>();
+        List<String> twitterServiceList = new ArrayList<String>();
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... screenNames) {
+            String result = null;
+
+            if (screenNames.length > 0) {
+                result = getTwitterStream(screenNames[0]);
+            }
+            return result;
+        }
+        // onPostExecute convert the JSON results into a Twitter object (which is an Array list of tweets
+        @Override
+        protected void onPostExecute(String result) {
+            Log.e("result",result);
+
+            try {
+                JSONArray jsonArray_data = new JSONArray(result);
+                for (int i=0; i<jsonArray_data.length();i++){
+
+                    JSONObject jsonObject = jsonArray_data.getJSONObject(i);
+                    twitterList.add(jsonObject.getString("text"));
+                }
+                Log.d("TWITTERLIST", twitterList.get(0));
+                Log.d("TWITTERLIST", twitterList.get(4));
+                for(String s : twitterList){
+                    if(s.contains("[NSL]")){
+                        twitterServiceList.add(s);
+                        Log.d("CONTAINED", s);
+                    }
+                }
+                if (twitterServiceList.get(0).contains("commenced")||twitterServiceList.get(0).contains("CLEARED")||twitterServiceList.get(0).contains("restored")||twitterServiceList.get(0).contains("resumed")){
+                    Log.d("NO FAULT", "NO FAULT");
+                }
+                else if (twitterServiceList.get(0).contains("due to")||twitterServiceList.get(0).contains("pls add")||twitterServiceList.get(0).contains("train travel time")||twitterServiceList.get(0).contains("no train service")){
+                    Log.d("GOT FAULT", "GOT FAULT");
+                }
+                else{
+                    return;
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+
+        // convert a JSON authentication object into an Authenticated object
+        private Authenticated jsonToAuthenticated(String rawAuthorization) {
+            Authenticated auth = null;
+            if (rawAuthorization != null && rawAuthorization.length() > 0) {
+                try {
+                    Gson gson = new Gson();
+                    auth = gson.fromJson(rawAuthorization, Authenticated.class);
+                } catch (IllegalStateException ex) {
+                    // just eat the exception
+                }
+            }
+            return auth;
+        }
+
+        private String getResponseBody(HttpRequestBase request) {
+            StringBuilder sb = new StringBuilder();
+            try {
+
+                DefaultHttpClient httpClient = new DefaultHttpClient(new BasicHttpParams());
+                HttpResponse response = httpClient.execute(request);
+                int statusCode = response.getStatusLine().getStatusCode();
+                String reason = response.getStatusLine().getReasonPhrase();
+
+                if (statusCode == 200) {
+
+                    HttpEntity entity = response.getEntity();
+                    InputStream inputStream = entity.getContent();
+
+                    BufferedReader bReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+                    String line = null;
+                    while ((line = bReader.readLine()) != null) {
+                        sb.append(line);
+                    }
+                } else {
+                    sb.append(reason);
+                }
+            } catch (UnsupportedEncodingException ex) {
+            }  catch (IOException ex2) {
+            }
+            return sb.toString();
+        }
+
+        private String getTwitterStream(String screenName) {
+            String results = null;
+
+            // Step 1: Encode consumer key and secret
+            try {
+                // URL encode the consumer key and secret
+                String urlApiKey = URLEncoder.encode(CONSUMER_KEY, "UTF-8");
+                String urlApiSecret = URLEncoder.encode(CONSUMER_SECRET, "UTF-8");
+
+                // Concatenate the encoded consumer key, a colon character, and the
+                // encoded consumer secret
+                String combined = urlApiKey + ":" + urlApiSecret;
+
+                // Base64 encode the string
+                String base64Encoded = Base64.encodeToString(combined.getBytes(), Base64.NO_WRAP);
+
+                // Step 2: Obtain a bearer token
+                HttpPost httpPost = new HttpPost(TwitterTokenURL);
+                httpPost.setHeader("Authorization", "Basic " + base64Encoded);
+                httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+                httpPost.setEntity(new StringEntity("grant_type=client_credentials"));
+                String rawAuthorization = getResponseBody(httpPost);
+                Authenticated auth = jsonToAuthenticated(rawAuthorization);
+
+                // Applications should verify that the value associated with the
+                // token_type key of the returned object is bearer
+                if (auth != null && auth.token_type.equals("bearer")) {
+
+                    // Step 3: Authenticate API requests with bearer token
+                    HttpGet httpGet = new HttpGet(TwitterStreamURL + screenName);
+
+                    // construct a normal HTTPS request and include an Authorization
+                    // header with the value of Bearer <>
+                    httpGet.setHeader("Authorization", "Bearer " + auth.access_token);
+                    httpGet.setHeader("Content-Type", "application/json");
+                    // update the results with the body of the response
+                    results = getResponseBody(httpGet);
+                }
+            } catch (UnsupportedEncodingException ex) {
+            } catch (IllegalStateException ex1) {
+            }
+            return results;
+        }
+    }
+
 }
