1d95cf4f6b731328b05225edb045098ae79ad00d
Anraza Val'tieri <jzcxw13@gmail.com>
2018-07-16 22:49:51 +0800

    Minor update and a crash fix
    Versioning updates as the build change.
    Crash fix related to Log.d("LookUpTrafficDuration", mrtLine);

--- app/src/main/java/com/sit/itp_team_9_smartandconnectedbusstops/AboutActivity.java
+++ app/src/main/java/com/sit/itp_team_9_smartandconnectedbusstops/AboutActivity.java
@@ -1,13 +1,17 @@
 package com.sit.itp_team_9_smartandconnectedbusstops;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
     private Button mButtonBack;
+    private TextView mVersion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +25,15 @@ public class AboutActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        mVersion = findViewById(R.id.version);
+        try {
+            PackageInfo pInfo = this.getPackageManager().getPackageInfo(getPackageName(), 0);
+            String version = pInfo.versionName;
+            mVersion.setText("Version: "+version);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
